9896a7c3730ee842c6ea713ca155caad70d08a4a
ToshiroAkihabara <toshiro.akihabara@gmail.com>
2023-12-12 00:36:17 +0300

    add os-win compatible func

--- frame.py
+++ frame.py
@@ -1,6 +1,7 @@
-from PIL import Image
+from typing import Any
 
-import customtkinter
+import customtkinter  # type: ignore
+from PIL import Image
 
 
 class ImageFrame(customtkinter.CTkFrame):
@@ -12,32 +13,35 @@ class ImageFrame(customtkinter.CTkFrame):
             dark_image=Image.open(self.__IMAGE_PATH), size=(250, 150)
         )
         self.label = customtkinter.CTkLabel(master=self, image=self.image, text="")
-        self.label.grid(row=1, column=0, padx=0, pady=(0, 0))
+        self.label.grid(row=1, column=0, padx=30, pady=(0, 0))
+
 
 class SelectWidgetFrame(customtkinter.CTkFrame):
-    def __init__(self, master, title: str, values: list[str]) -> None:
-        super().__init__(master)
+    def __init__(self, master, title: Any, values: list[str]) -> None:
         self.title = title
         self.values = values
-        self.title = customtkinter.CTkLabel(self, text=self.title, fg_color="transparent")
+        super().__init__(master)
+        self.title = customtkinter.CTkLabel(
+            self, text=self.title, fg_color="transparent"
+        )
         self.title.grid(row=0, column=0, padx=70, pady=(0, 10), sticky="w")
-        self.option = customtkinter.CTkComboBox(master=self, values=self.values)   
+        self.option = customtkinter.CTkComboBox(master=self, values=self.values)
         self.option.grid(row=2, column=0, padx=80, pady=(0, 10), sticky="w")
 
     def get(self):
         return self.option.get()
-    
+
+
 class RunButtonFrame(customtkinter.CTkFrame):
     def __init__(self, master, title: str, command) -> None:
-        super().__init__(master)
         self.title = title
         self.command = command
+        super().__init__(master)
         self.button_img = Image.open(r"image/light.png")
         self.select = customtkinter.CTkButton(
-            master=self, 
+            master=self,
             text=self.title,
             command=self.command,
-            image=customtkinter.CTkImage(dark_image=self.button_img)
+            image=customtkinter.CTkImage(dark_image=self.button_img),
         )
-        self.select.grid(row=3, column=0, padx=80, pady=(0, 20), sticky="w")
-    
+        self.select.grid(row=3, column=0, padx=80, pady=(0, 30), sticky="w")
--- shutdown.py
+++ shutdown.py
@@ -1,57 +1,46 @@
-from typing import Optional
-
-import subprocess
 import os
+import subprocess
+from typing import Optional
 
 
 class PowerManager:
-    def __init__(self, shutdown, reboot, logout, name) -> None:
-        self.name = name
+    def __init__(self, shutdown, reboot, logout) -> None:
         self.shutdown = shutdown
         self.reboot = reboot
         self.logout = logout
 
     def power_off(self):
-        # subprocess.call(self.shutdown)
-        print(f"Выключаю {self.name}. Code: {self.shutdown}")
+        subprocess.call(self.shutdown)
 
     def log_out(self):
-        # subprocess.call(self.logout)
-        print(f"Выхожу из {self.name}. Code: {self.logout}")
+        subprocess.call(self.logout)
 
     def restart(self):
-        # subprocess.call(self.reboot)
-        print(f"Перезагружаю {self.name}, Code: {self.reboot}")
+        subprocess.call(self.reboot)
+
 
 class PowerManagerLinux(PowerManager):
-    def __init__(self, name=None) -> None:
-        self.name = name
+    def __init__(self) -> None:
         self.shutdown = ["shutdown", "now"]
         self.reboot = ["shutdown", "-r", "now"]
         self.logout = ["gnome-session-quit", "--no-prompt"]
 
+
 class PowerManagerWindows(PowerManager):
-    def __init__(self, name=None) -> None:
-        self.name = name
-        self.shutdown = ["shutdown", "/s"]
-        self.reboot = ["shutdown", "/r"]
+    def __init__(self) -> None:
+        self.shutdown = ["shutdown", "/s", "/t", "0"]
+        self.reboot = ["shutdown", "/r", "/t", "0"]
         self.logout = ["shutdown", "/l"]
+
 
 class PowerManageDefiner:
     def __init__(self) -> None:
+        self.linux = PowerManagerLinux()
+        self.windows = PowerManagerWindows()
         self.type = os.name
-        self.name = self.get_name_os()
-        self.linux = PowerManagerLinux(self.name)
-        self.windows = PowerManagerWindows(self.name)
-        
+
     def get_manager_os(self) -> Optional[PowerManagerLinux | PowerManagerWindows]:
         if self.type == "posix":
             return self.linux
         else:
             return self.windows
-    
-    def get_name_os(self):
-        if self.type == "posix":
-            return "Linux"
-        else:
-            return "Windows"
